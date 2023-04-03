c05adad5b0c93af5f945e512cd1488cd5c083c0e
folosuru <folosuru@gmail.com>
2023-04-03 18:15:50 +0900

    initial commit

---
+++ api.php
@@ -0,0 +1,110 @@
+
+<?php
+/*
+if (array_key_exists("sort", $_GET)) {
+	$sort = $_GET["sort"];
+} else {
+	$sort = "domain";
+}
+
+// in php, true == 1
+// if paging = 1 , return value is spited
+if (array_key_exists("paging" , $_GET) and $_GET["paging"] == true) {
+	if (array_key_exists("offset", $_GET)) {
+		$offset = (int)$_GET["offset"];
+	} else {
+		$offset = 0;
+	}
+} else {
+
+}
+*/
+const sort_order = [
+	"asc" => "asc",
+	"desc" => "desc"
+];
+
+const sort_by = [
+	"name" => "name",
+	"user" => "user_count",
+	"post" => "post_count",
+	"domain" => "domain"
+];
+
+header("Content-Type: application/json; charset=utf-8");
+echo getAll();
+function getAll() : string{
+	$db = getPDO();
+	$request = $db->prepare("select * from instance_list where software = 'misskey' ". genInviteQuery() ." order by ".getSortBy() . " ". getSortOrder());
+	$request->execute();
+	$result = $request->fetchAll(PDO::FETCH_NAMED);
+	//var_dump($result);
+    $j= json_encode($result,JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT|JSON_INVALID_UTF8_IGNORE   );
+    return $j;
+}
+
+function getPDO(): PDO {
+	return new PDO(
+		'pgsql:dbname=misskey_tool host=127.0.0.1 port=5432',
+		'misskey_tool',
+		'test',
+		array(
+			PDO::ATTR_PERSISTENT => true
+		)
+	);
+}
+
+function genInviteQuery(): string {
+	if (array_key_exists("register",$_GET)){
+		$flag =	intval($_GET["register"]);
+	} else {
+		return "";
+	}
+
+	$result = "and (";
+	if ($flag & 0b1) { //everyone
+		$result .= "register = '0' or ";
+	}
+	if ($flag & 0b10) { //invite only
+		$result .= "register = '1' or ";
+	}
+	if ($flag & 0b100) { //need approval
+		$result .= "register = '2' or ";
+	}
+	if ($flag & 0b1000) { // deny
+		$result .= "register = '3' or ";
+	}
+	if ($flag & 0b10000) { //unknown
+		$result .= "register = '4' or ";
+	}
+	if ($result !== "and ("){
+		$result = substr($result,0,-4);
+		return $result.") ";
+	} else {
+		return "";
+	}
+
+}
+
+function getSortBy(): string {
+	if (!array_key_exists("sort_by",$_GET)){
+		return "post_count";
+	}
+	if (array_key_exists($_GET["sort_by"], sort_by)) {
+		return sort_by[$_GET["sort_by"]];
+	} else {
+		return "post_count";
+	}
+}
+
+function getSortOrder(){
+	if (!array_key_exists("sort_order",$_GET)){
+		return "";
+	} else {
+		if (array_key_exists($_GET["sort_order"], sort_order)){
+			return sort_order[$_GET["sort_order"]];
+		}
+	}
+	return "";
+}
+?>
